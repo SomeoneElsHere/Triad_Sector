6028b6329dc9a2073bb1b332a0ecfb61e680e2ed
SomeoneElsHere <161262730+SomeoneElsHere@users.noreply.github.com>
2026-06-15 21:24:23 -0500

    Update atmos.py

--- Scripts/atmos.py
+++ Scripts/atmos.py
@@ -10,7 +10,7 @@ os.chdir(pth)
 if not Path.exists(pth):
     os._exit
 
-num = 1000 #id num
+num = 13 #id num
 
 #main loop: for every file...
 for file in os.listdir(pth):
@@ -42,11 +42,11 @@ for file in os.listdir(pth):
         buf = buf.partition("id: ")[2]
         dat = buf.partition("\n")
         reagent += dat[0] + "\n\n"
-
+        
         buf = buf.partition("name: ")[2]
         dat = buf.partition("\n")
         name += dat[0] + "\n"
-
+        print(dat[0]+" = "+str(num)+",\n")
         if buf.find("metallic"):
             specificHeat += "0.5" + "\n" #https://www.engineersedge.com/materials/specific_heat_capacity_of_metals_13259.html, Btu/lb-C is around that number ehhh
             heatCapacityRatio += "1.0"+ "\n" #https://chem.libretexts.org/Bookshelves/Physical_and_Theoretical_Chemistry_Textbook_Maps/Thermodynamics_and_Chemical_Equilibrium_(Ellgen)/07%3A_State_Functions_and_The_First_Law/7.14%3A_Heat_Capacities_of_Solids-_the_Law_of_Dulong_and_Petit cp/cv ~= 1
@@ -61,6 +61,8 @@ for file in os.listdir(pth):
 
         f.write("- type: gas\n"+"  "+id+"  "+name+"  "+specificHeat+"  "+heatCapacityRatio+"  "+molarMass+"  "+color+"  "+reagent) #write it all!
         num = num +1
+        if num > 127:
+            num = -128
         f.flush()
     f.close()
     nf.close()
